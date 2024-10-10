5c13abe60f04930deaba78dba7983a1622989dc1
Jorge Chafloque Millones <37223352+jchafloquem@users.noreply.github.com>
2024-10-10 13:55:51 -0500

    Avance al 10%

---
+++ src/app/acustico/acustico.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-acustico',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './acustico.component.html',
+  styleUrl: './acustico.component.css'
+})
+export default class AcusticoComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,42 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path:'acustico',
+    loadComponent:() => import('./acustico/acustico.component'),
+    children:[
+      {
+        path:'home',
+        loadComponent:() => import('./acustico/pages/home/home.component')
+      },
+      {
+        path:'podcast',
+        loadComponent:() => import('./acustico/pages/podcast/podcast.component')
+      },
+      {
+        path:'noticias',
+        loadComponent:() => import('./acustico/pages/noticias/noticias.component')
+      },
+      {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+      },
+      {
+        path:'**',
+        redirectTo:'home',
+        pathMatch:'full'
+      }
+    ]
+  },
+  {
+    path:'',
+    redirectTo: 'acustico',
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    redirectTo: 'acustico',
+    pathMatch:'full'
+  }
+];
