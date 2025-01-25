9c9f9d9695392f11473eb00238d073bd375553db
Jorge Chafloque Millones <37223352+jchafloquem@users.noreply.github.com>
2025-01-25 14:26:42 -0500

    Agregar reproductor

--- src/app/acustico/pages/home/home.component.ts
+++ src/app/acustico/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { PlayerComponent } from "../player/player.component";
 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, PlayerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
