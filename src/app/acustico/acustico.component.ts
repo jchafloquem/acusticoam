19147554aec35783072e6574817210543f14ebc9
Jorge Chafloque Millones <37223352+jchafloquem@users.noreply.github.com>
2025-01-25 14:47:17 -0500

    Whatsapp

--- src/app/acustico/acustico.component.ts
+++ src/app/acustico/acustico.component.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 
 import { NavbarComponent } from '../shared/navbar/navbar.component';
 import { FooterComponent } from "../shared/footer/footer.component";
+import { WhatsappComponent } from "./pages/whatsapp/whatsapp.component";
 
 @Component({
   selector: 'app-acustico',
   standalone: true,
-  imports: [RouterModule, NavbarComponent, FooterComponent],
+  imports: [RouterModule, NavbarComponent, FooterComponent, WhatsappComponent],
   templateUrl: './acustico.component.html',
   styleUrl: './acustico.component.css'
 })
--- src/app/acustico/pages/home/home.component.ts
+++ src/app/acustico/pages/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { PlayerComponent } from "../player/player.component";
+import { WhatsappComponent } from "../whatsapp/whatsapp.component";
 
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule, PlayerComponent],
+  imports: [RouterModule, PlayerComponent, WhatsappComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
